ccfa9207cc95afa3a395e1cedfea1a93c50e226c
Alberto Mori <albi.mo87@hotmail.it>
2019-03-21 18:07:15 +0100

    add package.json and webpack first configuration

---
+++ VueStarter.AspNetCore/webpack.config.js
@@ -0,0 +1,32 @@
+﻿"use strict";
+
+const path = require("path");
+const outputDir = "./wwwroot";
+
+const isDevMode = process.env.NODE_ENV !== 'production';
+
+module.exports = {
+    mode: isDevMode ? "development" : "production",
+    resolve: {
+        modules: ['./node_modules'],
+        extensions: ['.js'],
+        alias: {
+            "@": path.resolve(__dirname, "Client")
+        }
+    },
+    entry: {
+        "main-client": "./Client/main.js"
+    },
+    output: {
+        path: path.resolve(__dirname, outputDir),
+        filename: "js/[name].js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js?$/,
+                loader: "babel-loader"
+            }
+        ]
+    }
+};
