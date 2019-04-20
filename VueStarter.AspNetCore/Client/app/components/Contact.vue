febbbb45ab6236287e08af645844127b04c906f2
Alberto Mori <albi.mo87@hotmail.it>
2019-04-20 13:45:04 +0200

    add api call

--- VueStarter.AspNetCore/Client/app/components/Contact.vue
+++ VueStarter.AspNetCore/Client/app/components/Contact.vue
@@ -1,19 +1,64 @@
 ﻿<template>
-    <section class="hero welcome is-light is-bold is-small">
-        <div class="hero-body">
-            <div class="container">
-                <h1 class="title">
-                    Contact
-                </h1>
-                <h2 class="subtitle">
-                    Feel free to fork this project from <a href="https://github.com/albx/VueStarter.AspNetCore" target="_blank">GitHub</a> and submit your contrib
-                </h2>
+    <div>
+        <section class="hero welcome is-light is-bold is-small">
+            <div class="hero-body">
+                <div class="container">
+                    <h1 class="title">
+                        Contact
+                    </h1>
+                    <h2 class="subtitle">
+                        Feel free to fork this project from <a href="https://github.com/albx/VueStarter.AspNetCore" target="_blank">GitHub</a> and submit your contrib
+                    </h2>
+                </div>
             </div>
-        </div>
-    </section>
+        </section>
+        <section class="section">
+            <div class="container">
+                <h3 class="title">Contacts (Loaded from API)</h3>
+                <ul>
+                    <li>
+                        <span>
+                            <i class="fab fa-github" aria-hidden="true"></i>
+                            {{ contacts.github }}
+                        </span>
+                    </li>
+                    <li>
+                        <span>
+                            <i class="fab fa-twitter" aria-hidden="true"></i>
+                            {{ contacts.twitter }}
+                        </span>
+                    </li>
+                    <li v-for="email in contacts.emails" v-bind:key="email">
+                        <span>
+                            <i class="far fa-envelope" aria-hidden="true"></i>
+                            {{ email }}
+                        </span>
+                    </li>
+                </ul>
+            </div>
+        </section>
+    </div>
 </template>
 <script>
+    import axios from 'axios'
+
     export default {
-        name: 'Contact'
+        name: 'Contact',
+        data() {
+            return {
+                contacts: {}
+            }
+        },
+        methods: {
+            loadContacts() {
+                axios.get('/api/contacts')
+                    .then((response) => {
+                        this.contacts = response.data || {}
+                    })
+            }
+        },
+        mounted() {
+            this.loadContacts()
+        }
     }
 </script>
---
+++ VueStarter.AspNetCore/Controllers/ContactsController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace VueStarter.AspNetCore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var contacts = new
+            {
+                github = "https://github.com/albx/VueStarter.AspNetCore",
+                twitter = "https://twitter.com/albx87",
+                emails = new string[] { "albi.mori@mail.com", "info@morialberto.it" }
+            };
+
+            return Ok(contacts);
+        }
+    }
+}
